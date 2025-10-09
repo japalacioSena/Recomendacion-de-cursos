7deeb57ce18ec2e73ddc0471b05d8c0f7dbcef66
japalacioSena <japalacio@sena.eu.co>
2025-10-09 11:03:12 -0500

    Cambio de mensajes en inicio

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,7 +8,8 @@ export default function Home() {
         <a
           href=""
           target="_blank"
-          className="inline-flex items-center font-semibold text-indigo-700">
+          className="inline-flex items-center font-semibold text-indigo-700"
+        >
           <div>
             <div className="flex items-center">
               <svg
@@ -30,8 +31,9 @@ export default function Home() {
             </div>
 
             <p className="mt-4 text-gray-500 text-sm leading-relaxed">
-              Accede a la vista de los cursos activos o que estan por salir y que son de tu interes para que puedas
-              ingresar a betowa e inscribirte.
+              Accede a la vista de los cursos activos o que estan por salir y
+              que son de tu interes para que puedas ingresar a betowa e
+              inscribirte.
             </p>
 
             <p className="mt-4 text-sm">
@@ -51,36 +53,36 @@ export default function Home() {
           </div>
         </a>
 
-        <div>
-          <div className="flex items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              className="w-6 h-6 stroke-gray-400"
-            >
-              <path
-                strokeLinecap="round"
-                d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
-              />
-            </svg>
-            <h2 className="ms-3 text-xl font-semibold text-gray-900">
-              Repositori
-            </h2>
-          </div>
+        <a
+          href=""
+          className="inline-flex items-center font-semibold text-indigo-700"
+        >
+          <div>
+            <div className="flex items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                className="w-6 h-6 stroke-gray-400"
+              >
+                <path
+                  strokeLinecap="round"
+                  d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
+                />
+              </svg>
+              <h2 className="ms-3 text-xl font-semibold text-gray-900">
+                Selección cursos de seguimiento
+              </h2>
+            </div>
 
-          <p className="mt-4 text-gray-500 text-sm leading-relaxed">
-            Este módulo se encarga de la gestión completa del inventario de
-            semillas para el sistema del SENA. Las principales funcionalidades
-            incluyen s
-          </p>
+            <p className="mt-4 text-gray-500 text-sm leading-relaxed">
+              Este módulo permite la selección y seguimiento de cursos o areás
+              de interés, en los que esten interesados los usuarios. Para cuando
+              hablan los cursos, se les notifique a los usuarios.
+            </p>
 
-          <p className="mt-4 text-sm">
-            <a
-              href=""
-              className="inline-flex items-center font-semibold text-indigo-700"
-            >
+            <p className="mt-4 text-sm">
               Ver
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -93,41 +95,40 @@ export default function Home() {
                   clipRule="evenodd"
                 />
               </svg>
-            </a>
-          </p>
-        </div>
-
-        <div>
-          <div className="flex items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              className="w-6 h-6 stroke-gray-400"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-              />
-            </svg>
-            <h2 className="ms-3 text-xl font-semibold text-gray-900">
-              Registro de Cambios
-            </h2>
+            </p>
           </div>
+        </a>
 
-          <p className="mt-4 text-gray-500 text-sm leading-relaxed">
-            Accede a nuestros reportes para obtener información detallada sobre:
-            Registro de cambios, auditoría, versión de semillas y archivos
-            comprimidos, estos reportes te ofrecen una visión completa de las
-            operaciones, y cambios realizados en el sistema.
-          </p>
-          <p className="mt-4 text-sm">
-            <a
-              href=""
-              className="inline-flex items-center font-semibold text-indigo-700"
-            >
+        <a
+          href=""
+          className="inline-flex items-center font-semibold text-indigo-700"
+        >
+          <div>
+            <div className="flex items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                className="w-6 h-6 stroke-gray-400"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+                />
+              </svg>
+              <h2 className="ms-3 text-xl font-semibold text-gray-900">
+                Selección de cursos de distribución
+              </h2>
+            </div>
+
+            <p className="mt-4 text-gray-500 text-sm leading-relaxed">
+              Este módulo permite dejar la lista de cursos que te recomienda el
+              sistema, de acuerdo a los cursos que haz realizado. Puedes
+              eliminar o agregar cursos a esta lista.
+            </p>
+            <p className="mt-4 text-sm">
               Ver
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -140,39 +141,39 @@ export default function Home() {
                   clipRule="evenodd"
                 />
               </svg>
-            </a>
-          </p>
-        </div>
-
-        <div>
-          <div className="flex items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              className="w-6 h-6 stroke-gray-400"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
-              />
-            </svg>
-            <h2 className="ms-3 text-xl font-semibold text-gray-900">
-              Usuarios
-            </h2>
+            </p>
           </div>
-          <p className="mt-4 text-gray-500 text-sm leading-relaxed">
-            Este módulo se encarga de la administración completa de los usuarios
-            en la plataforma. Las principales funcionalidades incluyen la
-            creación de usuarios y la asignación de roles del sistema.
-          </p>
-          <p className="mt-4 text-sm">
-            <a
-              href=""
-              className="inline-flex items-center font-semibold text-indigo-700"
-            >
+        </a>
+
+        <a
+          href=""
+          className="inline-flex items-center font-semibold text-indigo-700"
+        >
+          <div>
+            <div className="flex items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                className="w-6 h-6 stroke-gray-400"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
+                />
+              </svg>
+              <h2 className="ms-3 text-xl font-semibold text-gray-900">
+                Información de interés Betowa
+              </h2>
+            </div>
+            <p className="mt-4 text-gray-500 text-sm leading-relaxed">
+              Este módulo se encarga de mostrar información relevante sobre
+              Betowa, como noticias, actualizaciones, eventos próximos y otros
+              datos que puedan ser de interés para los usuarios.
+            </p>
+            <p className="mt-4 text-sm">
               Ver
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -185,9 +186,9 @@ export default function Home() {
                   clipRule="evenodd"
                 />
               </svg>
-            </a>
-          </p>
-        </div>
+            </p>
+          </div>
+        </a>
       </div>
     </div>
   );
